c986e1bbad225c007a9a9a82c4adb03c44bf0696
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-01 13:20:23 +0530

    TYpeWriter

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	export let name: string;
+	import Typewriter from './Typewriter.svelte'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Typewriter text="I am Pranav !"></Typewriter>
 </main>
 
 <style>
---
+++ src/Typewriter.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    export let text:string | number | undefined;
+    
+    let value:string = "";
+    let counterIndex = 0;
+
+    function createTypewriterEffect() {    
+        let speed = 50;
+        if(counterIndex < String(text).length){
+            value += String(text).charAt(counterIndex)
+            counterIndex += 1
+            setTimeout(() => {
+                console.log(value)
+                createTypewriterEffect()
+            }, 100)
+        }
+    }
+
+    createTypewriterEffect()
+</script>
+
+<main>
+    <div class="text">
+        <div class="type">
+            {#if text}
+                <p class="data">
+                    {value}
+                </p>
+            {/if}
+        </div>
+    </div>
+</main>
+
+<style>
+
+</style>
