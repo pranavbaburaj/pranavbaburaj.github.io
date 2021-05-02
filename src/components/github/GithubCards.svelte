4fdc3f6cf37b8a5d1906ef84f7374d67c0c3173a
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-02 20:26:38 +0530

    Add all the files

--- src/components/github/GithubCards.svelte
+++ src/components/github/GithubCards.svelte
@@ -1,57 +1,71 @@
 <script lang="ts">
+  let data = window.localStorage.getItem('repos')
+  if (!data){
+    window.location.reload()
+  }
+
+  data = JSON.parse(data).data
+  if(!Array.isArray(data)){
+    window.location.reload()
+  }
+
+  let show = Array.isArray(data)
+
 </script>
 
 <main>
 
 <div class="github-cards">
-  <a href="https://github.com/Nexmo/nexmo-ruby" class="github-card" data-github="Nexmo/nexmo-ruby">
-    <h3>nexmo-ruby</h3>
-    <p>Nexmo REST API client for Ruby</p>
-    <span class="github-card__meta">
-      <span class="github-card__language-icon" style="color: #7A0410;">●</span> Ruby
-    </span>
-    <span class="github-card__meta">
-      <i class="fa fa-star" aria-hidden="true"></i>
-      <span data-stars>
-        <i class="fa fa-spinner" aria-hidden="true"></i>
+{#if show}
+  {#each data as repo}
+    <a class="github-card" data-github="Nexmo/nexmo-ruby" href="" on:click={(event) => {
+      window.open(repo.html_url)
+    }}>
+      <h3>{repo.name}</h3>
+      <p>{repo.description}</p>
+      <span class="github-card__meta">
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span data-stars>
+          {repo.stargazers_count}
+        </span>
       </span>
-    </span>
-    <span class="github-card__meta">
-      <i class="fa fa-code-fork" aria-hidden="true"></i>
-      <span data-forks>
-        <i class="fa fa-spinner" aria-hidden="true"></i>
-      </span>
-    </span>
-  </a>
+    </a>
+  {/each}
+{/if}
 </div>
 </main>
 
 <style lang="css">
 
+
 .github-cards {
   display: flex;
   flex-flow: row;
   flex-wrap: wrap;
-  width: 900px;
+  width: 5vh;
+  
 }
 
 .github-card {
+  background : inherit;
   display: block;
   box-sizing: border-box;
-  border: 1px solid #ccc;
+  border: 1px solid rgb(65, 63, 63);
   margin: 10px;
-  padding: 20px;
-  color: #555;
+  padding: 16px;
+  color: rgb(139, 148, 158);
   text-decoration: none;
-  font-size: 13px;
+  font-size: 14px;
   flex: 1;
   min-width: 250px;
+  border-radius : 10px;
 }
 
 h3 {
     margin-top: 0;
     color: #4078c0;
-    font-size: 15px;
+    font-size: 16px;
+    font-weight : 600;
   }
 
 .github-card__meta {
