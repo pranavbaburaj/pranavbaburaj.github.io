aeac0e75c627d338e6c8ee479c24ef66cc8a5758
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-02 13:30:36 +0530

    Formatted and commented the code

--- repos/main.py
+++ repos/main.py
@@ -3,7 +3,6 @@ import os
 import requests
 
 from flask import Flask, views, jsonify, request, abort
-
 """
 Load all the environment variabled
 from the .env file. The env file
@@ -16,6 +15,7 @@ USERNAME = "pranavbaburaj"
 
 app = Flask(__name__)
 
+
 class ArrangeProjects(object):
     limit = 3
 
@@ -26,10 +26,30 @@ class ArrangeProjects(object):
         self.projects = []
 
     def find_top_stars(self, projects):
-        data = sorted([int(key) for key in projects.keys()])    
+        """
+        Get the number of stars in each and every project
+        and sort it as ana array and returns the reversed
+        array
+
+        Args:
+            projects ([dict]): A dict containing the number of stars
+            and the array of projects with the corresponding number of stars
+
+        Returns:
+            list: The stars gained by the projects sorted in 
+            descending order
+        """
+        data = sorted([int(key) for key in projects.keys()])
         return data[::-1]
 
     def find_top_projects(self):
+        """
+        Return the top three projects based 
+        on the number of stars
+
+        Returns:
+            list: The list of top projects
+        """
         for index in range(len(self.top_indexes)):
             current_index = str(self.top_indexes[index])
             for project in self.project_list.get(current_index):
@@ -41,15 +61,11 @@ class ArrangeProjects(object):
         return self.projects
 
 
-class GithubResponse(object):        
-    headers = {"Accept":"application/vnd.github.mercy-preview+json"} 
+class GithubResponse(object):
+    headers = {"Accept": "application/vnd.github.mercy-preview+json"}
     required_fields = [
-        "name",
-        "html_url",
-        "description",
-        "homepage",
-        "stargazers_count"
-    ]     
+        "name", "html_url", "description", "homepage", "stargazers_count"
+    ]
 
     def __init__(self, username, api_key):
         self.username = username
@@ -57,15 +73,11 @@ class GithubResponse(object):
 
     def create_request(self):
         repos = ArrangeProjects(
-                self.remove_unwanted_fields(
-                requests.get(
-                self.get_api_url(self.username),
-                headers=self.headers, 
-                auth=(self.username, self.api_key)
-            ).json(),
-            self.required_fields
-            )
-        ).find_top_projects()
+            self.remove_unwanted_fields(
+                requests.get(self.get_api_url(self.username),
+                             headers=self.headers,
+                             auth=(self.username, self.api_key)).json(),
+                self.required_fields)).find_top_projects()
 
         return repos
 
@@ -81,7 +93,7 @@ class GithubResponse(object):
 
     def assign_indexes(self, data):
         return_value = {}
-        for index,element in enumerate(data):
+        for index, element in enumerate(data):
             stars = element["stargazers_count"]
             if not return_value.get(str(stars)):
                 return_value[str(stars)] = []
@@ -91,14 +103,15 @@ class GithubResponse(object):
     def get_api_url(self, username):
         return f"https://api.github.com/users/{self.username}/repos"
 
+
 class IndexView(views.MethodView):
     def post(self):
         return jsonify(status=404, message="post not supported")
 
     def get(self):
         response = GithubResponse(USERNAME, TOKEN).create_request()
-        return jsonify({"data" : response})
+        return jsonify({"data": response})
+
 
 app.add_url_rule("/", view_func=IndexView.as_view("index"))
 app.run(debug=True)
-
--- repos/test.py
+++ repos/test.py
@@ -2,15 +2,13 @@ import requests
 import json
 
 url = 'http://localhost:5000/'
-headers = {
-  'Content-Type': 'application/json'
-}
-data = json.dumps({"api_key" : "LOL"})
+headers = {'Content-Type': 'application/json'}
+data = json.dumps({"api_key": "LOL"})
 response = requests.request(
-  'GET',
-  'http://localhost:5000/',
-  data=data,
-  headers=headers,
+    'GET',
+    'http://localhost:5000/',
+    data=data,
+    headers=headers,
 )
 
 print(json.loads(response.content))
