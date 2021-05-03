d30180545bacac0b8fcb383dc6129ef5b9637e18
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-03 11:36:02 +0530

    Callout

---
+++ src/components/Callout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <div class="callout">
+        <div class="callout-header">Callout Header</div>
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        <div class="callout-container">
+          <p>Some text...</p>
+        </div>
+    </div>
+</main>
+
+<style>
+
+</style>
