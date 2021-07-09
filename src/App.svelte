32bee1b59acf155a9427f574094daa006e045507
Pranav Baburaj <diamonddevstuff@gmail.com>
2021-07-09 17:22:03 +0000

    get rickroled

--- src/App.svelte
+++ src/App.svelte
@@ -85,11 +85,12 @@
       },
     ],
   ]);
-
+  let f5ConfirmationModelDisplay = "none"
 
   function listenKeyboardEvent(event) {
     if(event.key == "F5"){
       event.preventDefault()
+      f5ConfirmationModelDisplay = "block"
     }
   }
 
@@ -112,6 +113,22 @@
     event.preventDefault();
   }}
 >
+  <div id="id01" class="modal" style="display:{f5ConfirmationModelDisplay};">
+    <span>&times;</span>
+    <form class="modal-content">
+      <div class="container">
+        <h1>Are you sure?</h1>
+        <!-- <p>Are you sure you want to delete your account?</p> -->
+
+        <div class="clearfix">
+          <button type="button" class="cancelbtn" on:click={() => {
+            f5ConfirmationModelDisplay = "none"
+          }}>Cancel</button>
+          <button type="button" class="deletebtn" on:click={() => {window.open("https://www.youtube.com/watch?v=eBGIQ7ZuuiU")}}>Proceed</button>
+        </div>
+      </div>
+    </form>
+  </div>
   <Profile data={JSON.parse(userData)} />
   <Typewriter text="Hey, I'm  Pranav" emoji="👋🏾" />
   <SocialLinks links={socialLinkList} />
@@ -130,4 +147,100 @@
   :root {
     background-color: #24252a;
   }
+
+  /* Set a style for all buttons */
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+button:hover {
+  opacity:1;
+}
+
+/* Float cancel and delete buttons and add an equal width */
+.cancelbtn, .deletebtn {
+  float: left;
+  width: 50%;
+}
+
+/* Add a color to the cancel button */
+.cancelbtn {
+  background-color: #ccc;
+  color: black;
+}
+
+/* Add a color to the delete button */
+.deletebtn {
+  background-color: #6B88EF;
+}
+
+/* Add padding and center-align text to the container */
+.container {
+  padding: 16px;
+  text-align: center;
+}
+
+/* The Modal (background) */
+.modal {
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: #24252A;
+  padding-top: 50px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* Style the horizontal ruler */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* The Modal Close Button (x) */
+.close {
+  position: absolute;
+  right: 35px;
+  top: 15px;
+  font-size: 40px;
+  font-weight: bold;
+  color: #f1f1f1;
+}
+
+.close:hover,
+.close:focus {
+  color: #f44336;
+  cursor: pointer;
+}
+
+/* Clear floats */
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+/* Change styles for cancel button and delete button on extra small screens */
+@media screen and (max-width: 300px) {
+  .cancelbtn, .deletebtn {
+    width: 100%;
+  }
+}
 </style>
